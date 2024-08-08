6fd1c5f0dd537bfc1efc180f2a9c44edfaf61505
Jillaz <127788124+Jillaz@users.noreply.github.com>
2024-08-08 20:06:55 +0300

    Try N2
    Поправлен скрипт Scaler
    Удалена переменная _newSize

--- Assets/Scripts/Scaler.cs
+++ Assets/Scripts/Scaler.cs
@@ -1,16 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Scaler : MonoBehaviour
 {
-    [SerializeField] private float _speed;
-    private float _newSize = 0f;
+    [SerializeField] private float _speed;    
 
     private void Update()
-    {
-        _newSize += _speed * Time.deltaTime;
-        transform.localScale += new Vector3(_newSize, _newSize, _newSize);
-       // transform.localScale += Vector3.up * _speed * Time.deltaTime;
+    {        
+        transform.localScale += Vector3.one * _speed * Time.deltaTime;
     }
 }
